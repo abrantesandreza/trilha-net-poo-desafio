84189e96e896538735b23c2ca1504c4a73f00d82
Andreza Abrantes <andrezaabrantes@gmail.com>
2023-11-03 23:39:13 -0300

    Classes Nokia e Iphone implementadas e Classe Smartphone finalizada

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("65465436546", "Air", "ASB558", 250);
+
+nokia.InstalarAplicativo("Github");
+
+Iphone iphone = new Iphone("352465161", "Iphone 23", "ASSD5566", 550);
+
+iphone.InstalarAplicativo("LinkedIn");
